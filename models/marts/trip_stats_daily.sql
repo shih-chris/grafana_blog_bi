f5615f407d371fb344310e04f090e3c2fb349a46
Chris Shih <chris.shih@grafana.com>
2022-01-04 18:45:21 +0000

    add time units to trip duration

--- models/marts/trip_stats_daily.sql
+++ models/marts/trip_stats_daily.sql
@@ -19,8 +19,8 @@ final as (
     select
         utils_date.date_stamp
         ,count(distinct trip_id) as total_trips
-        ,sum(trip_duration) as total_trip_duration
-        ,1.0 * sum(trip_duration) / count(distinct trip_id) as average_trip_duration
+        ,sum(trip_duration_seconds) / 60 as total_trip_duration_minutes
+        ,1.0 * sum(trip_duration_seconds) / count(distinct trip_id) / 60 as average_trip_duration_minutes
     from
         citibike_trips
         inner join utils_date on date_trunc(citibike_trips.trip_started_at, day) = utils_date.date_stamp
