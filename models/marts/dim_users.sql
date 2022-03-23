2473e7f4f245eb8c1395e24e5acee0552ff084f8
Chris Shih <chs820@gmail.com>
2022-03-23 13:55:28 -0400

    create dimension table for users

--- models/marts/dim_users.sql
+++ models/marts/dim_users.sql
@@ -10,7 +10,13 @@ citibike_trips as (
 final as (
 
     select
+        bike_id
+        ,birth_year
+        ,gender
+        ,trip_started_at
 
+        -- lets us pull the most recent data on a bike_id's birth_year and gender
+        ,row_number() over (partition by bike_id order by trip_started_at desc) as row_num
     from
         citibike_trips
 
@@ -18,3 +24,4 @@ final as (
 
 select *
 from final
+where row_num = 1
