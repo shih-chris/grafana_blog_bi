a72ff8d738d387256bb2d1c444cfae8a0579ac39
Chris Shih <chris.shih@grafana.com>
2022-03-23 20:57:47 +0000

    fix typos

--- models/marts/agg_trips_by_user_type_daily.sql
+++ models/marts/agg_trips_by_user_type_daily.sql
@@ -24,17 +24,17 @@ final as (
         ,1.0 * sum(fct_trips.trip_duration_minutes) / count(distinct fct_trips.trip_id) as average_trip_duration_minutes
 
         -- am trip stats
-        ,count(distinct case when extract(citibike_trips.trip_started_at, hour) < 12 then citibike_trips.trip_id) as am_trips
-        ,sum(distinct case when extract(citibike_trips.trip_started_at, hour) < 12 then citibike_trips.trip_duration_minutes) as am_trip_duration_minutes
-        ,1.0 * sum(distinct case when extract(citibike_trips.trip_started_at, hour) < 12 then citibike_trips.trip_duration_minutes) /
-            count(distinct case when extract(citibike_trips.trip_started_at, hour) < 12 then citibike_trips.trip_id)
+        ,count(distinct case when extract(hour from fct_trips.trip_started_at) < 12 then fct_trips.trip_id end) as am_trips
+        ,sum(distinct case when extract(hour from fct_trips.trip_started_at) < 12 then fct_trips.trip_duration_minutes end) as am_trip_duration_minutes
+        ,1.0 * sum(distinct case when extract(hour from fct_trips.trip_started_at) < 12 then fct_trips.trip_duration_minutes end) /
+            count(distinct case when extract(hour from fct_trips.trip_started_at) < 12 then fct_trips.trip_id end)
             as am_average_trip_duration_minutes
 
         -- pm trip stats
-        ,count(distinct case when extract(citibike_trips.trip_started_at, hour) >= 12 then citibike_trips.trip_id) as pm_trips
-        ,sum(distinct case when extract(citibike_trips.trip_started_at, hour) >= 12 then citibike_trips.trip_duration_minutes) as pm_trip_duration_minutes
-        ,1.0 * sum(distinct case when extract(citibike_trips.trip_started_at, hour) >= 12 then citibike_trips.trip_duration_minutes) /
-            count(distinct case when extract(citibike_trips.trip_started_at, hour) >= 12 then citibike_trips.trip_id)
+        ,count(distinct case when extract(hour from fct_trips.trip_started_at) >= 12 then fct_trips.trip_id end) as pm_trips
+        ,sum(distinct case when extract(hour from fct_trips.trip_started_at) >= 12 then fct_trips.trip_duration_minutes end) as pm_trip_duration_minutes
+        ,1.0 * sum(distinct case when extract(hour from fct_trips.trip_started_at) >= 12 then fct_trips.trip_duration_minutes end) /
+            count(distinct case when extract(hour from fct_trips.trip_started_at) >= 12 then fct_trips.trip_id end)
             as pm_average_trip_duration_minutes
 
     from
